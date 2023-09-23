4ade7854cf3964c6958692c467f680d94f51ac31
Justin Huynh <justinhuynh639@gmail.com>
2023-09-23 12:55:01 -0700

    completed madlibs generator

--- project2.py
+++ project2.py
@@ -1,8 +1,8 @@
-#assign variable to opening file
+#open file and read it
 with open("story.txt", "r") as f:
     story = f.read()
 
-#get unique elements
+#get unique words
 words = set()
 #find starting index of the word
 start_of_word = -1
@@ -10,7 +10,7 @@ start_of_word = -1
 target_start = "<"
 target_end = ">"
 
-#loop through our story, give us acces to the position of element in story
+#loop through our story, give us access to the position of element in story
 for i, char in enumerate(story):
     if char == target_start:
         start_of_word = i
@@ -29,4 +29,9 @@ for word in words:
     #create dictionary that has all the words associated with the value
     answers[word] = answer
 
-print(answers)
+#take every instance of the words and replace them in the story
+for word in words:
+    #replace all instances
+    story = story.replace(word, answers[word])
+
+print(story)
